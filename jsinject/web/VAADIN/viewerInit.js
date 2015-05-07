d8b18d4714903951a84259a81fd54ce9b9659ad6
vparedes <victor.paredes@coderoad.com>
2015-05-07 08:57:33 -0400

    CRTF-183 Aegis Variance Viewer Template Adjusting

--- jsinject/web/VAADIN/viewerInit.js
+++ jsinject/web/VAADIN/viewerInit.js
@@ -1,7 +1,7 @@
 /**
  * Created by carolasilvateran on 5/3/15.
  */
-com_tierconnect_jsplugins_viewerInit = function () {
+com_tierconnect_jsplugins_VarianceViewer = function () {
     $(document).ready(function($){
         jQuery(".ae-auto-init-ba").VarianceViewer();
         AE.ResizeEngine();
